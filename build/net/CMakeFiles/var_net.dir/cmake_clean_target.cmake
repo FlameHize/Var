0dda3d89dd45b8b8ad7f34ef3a1609751571ba51
FlameHize <18766876835@163.com>
2024-08-20 10:41:40 +0800

    first commit of company developer

--- build/net/CMakeFiles/var_net.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "../../lib/libvar_net.a"
-)
--- build/net/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zgx/project/Var/net/test
-# Build directory: /home/zgx/project/Var/build/net/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
